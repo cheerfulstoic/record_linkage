eff5420e11e69ded3990347dd5070cd7470a21b7
Brian Underwood <ml+github@semi-sentient.com>
2015-07-22 13:30:32 -0600

    Add let_context helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,14 @@ require 'bundler/setup'
 require 'rspec'
 require 'rspec/its'
 
-module TestHelpers
+# Introduces `let_context` helper method
+# This allows us to simplify the case where we want to
+# have a context which contains one or more `let` statements
+module LetContextHelpers
+  # Supports giving either a Hash or a String and a Hash as arguments
+  # In both cases the Hash will be used to define `let` statements
+  # When a String is specified that becomes the context description
+  # If String isn't specified, Hash#inspect becomes the context description
   def let_context(*args, &block)
     classes = args.map(&:class)
     context_string, hash =
@@ -25,5 +32,5 @@ module TestHelpers
 end
 
 RSpec.configure do |c|
-  c.extend TestHelpers
+  c.extend LetContextHelpers
 end
